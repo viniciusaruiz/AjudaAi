6781c6a8c448cbf3e6c26b7063551eb0cf1626fd
Alexandre Machado <alexandrem16@al.insper.edu.br>
2023-05-30 00:28:12 -0300

    datas. calendario-pt1

--- js/calendario.js
+++ js/calendario.js
@@ -1,24 +1,55 @@
 document.addEventListener("DOMContentLoaded", function(){
+    dic = {'FGV': [5,9,12,18],
+    'USP':[6,18,22,27],
+    'PUCSP':[9,18,24,28],
+    'MACKENZIE':[2,8,18,22],
+    'FAAP':[8,14,21,27],
+    'UNIFESP':[1,7,26,28],
+    'São Camilo':[3,10,15,25],
+    'UNICAMP':[11,16,24,28],
+    'Mauá':[10,19,24,28],
+    'Santa Casa':[5,10,21,27],
+    'Einstein':[1,10,20,22],
+    'FSLM':[14,18,22,28],
+    'UFSCAR':[4,8,15,19],
+    'UNISO':[2,9,20,26],
+    'Cásper Líbero':[10,15,20,25],
+    'Insper':[1,10,20,26]
+}
 
     // function getOption(){
     //     dropdown = document.querySelector('#meses').value
     //     console.log(dropdown)
     // if (dropdown=='jan' || dropdown=='mar' || dropdown=='mai' || dropdown=='jul' || dropdown=='ago' || dropdown=='out' || dropdown=='dez'){
-    lista = document.querySelector('.calendario')
-    li1 = document.createElement('li .dia')
-    li1.innerHTML = '29'
-    li2 = document.createElement('li .dia')
-    li2.innerHTML = '30'
-    li3 = document.createElement('li .dia')
-    li3.innerHTML = '31'
-    lista.appendChild(li1)
-    lista.appendChild(li2)
-    lista.appendChild(li3)
-        // }
+    // lista = document.querySelector('.calendario')
+    // li1 = document.createElement('li .dia')
+    // li1.innerHTML = '29'
+    // li2 = document.createElement('li .dia')
+    // li2.innerHTML = '30'
+    // li3 = document.createElement('li .dia')
+    // li3.innerHTML = '31'
+    // lista.appendChild(li1)
+    // lista.appendChild(li2)
+    // lista.appendChild(li3)
+    //     // }
     // }
     dias = document.querySelectorAll('.dia')
     fechar = document.querySelector('.fechar')
     janela = document.querySelector('.janela')
+
+    lista = localStorage.getItem('faculs').split(',')
+    ul = document.querySelector('ul')
+
+    dic2 = {}
+    for (item of lista){
+        i = 0
+        console.log(dic[item])
+        for (dia of dic[item]){
+            if (i == 0){
+                // aux[]
+            }
+        }
+    }
 
     for (dia of dias){
         dia.addEventListener('click', function(event){
@@ -28,5 +59,12 @@ document.addEventListener("DOMContentLoaded", function(){
     fechar.addEventListener('click', function(event){
         event.stopPropagation()
         janela.style.display = 'none'
+
+    // if (parseInt(dia.innerHTML) == )
     })
+
+
+
+
+
 })
--- js/ranking.js
+++ js/ranking.js
@@ -19,6 +19,7 @@ document.addEventListener("DOMContentLoaded", function(){
 
     selecao = document.querySelector('select')
     ranking = document.querySelectorAll('.tabelinha')
+
     top1 = ranking[2]
     top2 = ranking[3]
     top3 = ranking[4]
@@ -28,9 +29,25 @@ document.addEventListener("DOMContentLoaded", function(){
         top2.innerHTML =  '#2 '+ dicionario[selecao.value][1]
         top3.innerHTML =  '#3 '+ dicionario[selecao.value][2]
         top4.innerHTML =  '#4 '+ dicionario[selecao.value][3]
+    
+        checkboxes = document.querySelectorAll('input[type=checkbox]')
+        checkboxes[0].name = dicionario[selecao.value][0]
+        checkboxes[1].name = dicionario[selecao.value][1]
+        checkboxes[2].name = dicionario[selecao.value][2]
+        checkboxes[3].name = dicionario[selecao.value][3]
     })
 
-
+    checkboxes = document.querySelectorAll('input[type=checkbox]')
+    for (checkbox of checkboxes) {
+        checkbox.addEventListener('change', function(event){
+            chkboxs = document.querySelectorAll('input[type=checkbox]:checked');
+            lista = []
+            for (chkbox of chkboxs){
+                lista.push(chkbox.name)
+            }
+        localStorage.setItem('faculs',lista)
+        })
+    }
 
 
 
